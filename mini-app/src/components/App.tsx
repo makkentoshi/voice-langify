050e507974146fbf5f06b5e3dddc22ee52f40dfd
Mark <pochta.zdraste@mail.ru>
2025-05-14 22:42:55 +0500

    Back to prev version of App.tsx

--- mini-app/src/components/App.tsx
+++ mini-app/src/components/App.tsx
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from 'react';
+// src/components/App.tsx
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { init } from '@/init';
 import { Layout } from './Layout';
 import { BottomBar } from './BottomBar';
 import HomePage from '@/pages/HomePage';
@@ -10,22 +10,6 @@ import FlashcardsPage from '@/pages/spanish/FlashcardsPage';
 import GrammarPage from '@/pages/spanish/GrammarPage';
 
 export default function App() {
-  const [ready, setReady] = useState(false);
-
-  useEffect(() => {
-    init({ debug: false, eruda: false, mockForMacOS: false })
-      .then(() => setReady(true))
-      .catch((err) => {
-        console.error('Init SDK error:', err);
-        // можно переключить на экран ошибки или EnvUnsupported
-      });
-  }, []);
-
-  // Пока SDK не инициализировался, рендерим пустую заглушку (или Splash)
-  if (!ready) {
-    return null;
-  }
-
   return (
     <Router>
       <Routes>
