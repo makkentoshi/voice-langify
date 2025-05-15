eb76211a1f5cb721216e0966f28c7aa6456c5e23
Mark <pochta.zdraste@mail.ru>
2025-05-15 20:05:25 +0500

    Update index.css

--- mini-app/vite.config.ts
+++ mini-app/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import react from "@vitejs/plugin-react-swc";
 import mkcert from "vite-plugin-mkcert";
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
 // https://vitejs.dev/config/
@@ -23,7 +23,7 @@ export default defineConfig({
     // https://www.npmjs.com/package/vite-tsconfig-paths
     tsconfigPaths(),
 
-    tailwindcss(),  
+    tailwindcss(),
     // Creates a custom SSL certificate valid for the local machine.
     // Using this plugin requires admin rights on the first dev-mode launch.
     // https://www.npmjs.com/package/vite-plugin-mkcert
